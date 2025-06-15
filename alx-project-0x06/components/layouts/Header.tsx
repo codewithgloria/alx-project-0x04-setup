c08611eaa68470e08d5157e17e6e8f2af95f635b
Gloria <abinezagloria@gmail.com>
2025-06-15 19:36:06 +0300

    added redux state mgmt

---
+++ alx-project-0x06/components/layouts/Header.tsx
@@ -0,0 +1,37 @@
+'use client'; 
+
+import React from 'react';
+import Link from "next/link";
+import Button from "../common/Button";
+import { usePathname } from "next/navigation";
+import { useSelector, useDispatch } from "react-redux";
+import { RootState } from "@/store/store";
+
+const Header: React.FC = () => {
+  const pathname = usePathname();
+  const count = useSelector((state: RootState) => state.counter.value);
+
+  return (
+    <header className="fixed w-full bg-white shadow-md z-50">
+      <div className="container mx-auto flex justify-between items-center py-6 px-4 md:px-8">
+        <Link href="/" className="text-3xl md:text-5xl font-bold text-gray-800 tracking-tight">
+          Splash App
+        </Link>
+
+        {/* Button Group or Counter Display */}
+        <div className="flex gap-4 items-center">
+          {!["/counter-app"].includes(pathname) ? (
+            <>
+              <Button buttonLabel="Sign In" buttonBackgroundColor="red" />
+              <Button buttonLabel="Sign Up" buttonBackgroundColor="blue" />
+            </>
+          ) : (
+            <p className="font-semibold text-lg">Current count: {count}</p>
+          )}
+        </div>
+      </div>
+    </header>
+  );
+};
+
+export default Header;
